c890d7d6fb4a34b2dcdaee7ee14c6b4598357192
Cauê Netto <caue.netto234@gmail.com>
2025-02-03 15:09:53 -0300

    Adiciona estrutura básica da página de notas

---
+++ src/app/note/page.js
@@ -0,0 +1,23 @@
+"use client";
+
+import Menu from "@/ui/Menu";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+
+export default function Note() {
+  const router = useRouter();
+
+  const handleMenuClick = (page) => {
+    router.push(`/${page}`);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Menu onMenuClick={handleMenuClick} />
+      <div className={styles.content}>
+        <h1>Notas</h1>
+        <p>Aqui será a página de notas</p>
+      </div>
+    </div>
+  );
+}
