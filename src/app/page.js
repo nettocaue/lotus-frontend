968e8a8524f8987eae48a4272199288f0055f999
Cauê Netto <caue.netto234@gmail.com>
2025-02-03 15:09:14 -0300

    Adiciona estrutura básica da página principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
-export default function Home() {
+"use client";
+
+import Menu from "@/ui/Menu";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+
+const Home = () => {
+  const router = useRouter();
+
+  const handleMenuClick = (page) => {
+    router.push(`${page}`);
+  };
+
   return (
-    <main>
-      <div>Hello World!</div>
-    </main>
+    <div className={styles.container}>
+      <Menu onMenuClick={handleMenuClick} />
+      <div className={styles.content}>
+        <h1>Bem-vindo à Página Inicial</h1>
+        <p>Conteúdo dinâmico baseado no menu clicado</p>
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
