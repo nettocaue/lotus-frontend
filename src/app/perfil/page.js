6741468fc18db5b1fe11349e38ffdbd11d5f1906
Cauê Netto <caue.netto234@gmail.com>
2025-02-03 15:10:15 -0300

    Adiciona estrutura básica da página de perfil

---
+++ src/app/perfil/page.js
@@ -0,0 +1,23 @@
+"use client";
+
+import Menu from "@/ui/Menu";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+
+export default function Perfil() {
+  const router = useRouter();
+
+  const handleMenuClick = (page) => {
+    router.push(`/${page}`);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Menu onMenuClick={handleMenuClick} />
+      <div className={styles.content}>
+        <h1>Perfil</h1>
+        <p>Aqui será a página de perfil do usuário</p>
+      </div>
+    </div>
+  );
+}
